a3913847790dd2b4f9ed95d847c1d82c14f74bd2
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-02-15 11:41:36 +0800

    commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HelloWorldPage(),
+    );
+  }
+}
+
+
+class HelloWorldPage extends StatefulWidget {
+  @override
+  _HelloWorldPageState createState() => _HelloWorldPageState();
+}
+
+class _HelloWorldPageState extends State<HelloWorldPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+        actions: <Widget>[
+          Icon(Icons.add)
+        ],
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:Column(
+            children: <Widget>[
+              Text("Welcome to MyApp"),
+              Text("This is my first app"),
+            ]
+          )
+        )
+      ),
+    );
+  }
+}
